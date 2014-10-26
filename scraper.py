4f24b43a5b8d3127eb8af5cc993e4601b20e6eeb
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 02:17:06 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,23 +1,15 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
-
 import scraperwiki
-import lxml.html
-#
-# # Read in a page
-html = scraperwiki.scrape("http://www.archiv-buergerbewegung.de/index.php/demonstrationen")
-#
-# # Find something on the page using css selectors
-root = lxml.html.fromstring(html)
-root.cssselect("map")
-#
-# # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['title'], data={"bezirk": "Schwerin"})
-#
-# # An arbitrary query against the database
-scraperwiki.sql.select("* from data")
+from bs4 import BeautifulSoup # BeautifulSoup helps you to find what you want in HTML
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+url = "http://www.archiv-buergerbewegung.de/index.php/demonstrationen" # loading your url from the csv/database
+html = scraperwiki.scrape(url) # download the html content of the page
+#html = ""
+soup = BeautifulSoup(html) # load the html into beautifulsoup
+
+bezirke = [] # start with an empty list
+for bezirkarea in soup.find_all("area"): # for each 
+    url = bezirkarea['href']
+    title = bezirkarea['title']
+    bezirke.append({"url": url, "bezirk": title}) # put the values extracted into a list
+
+scraperwiki.sqlite.save(unique_keys=["url"], data=bezirkarea) # save the list of cat photos and captions
