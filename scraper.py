67ec167e437c1925c5229e292bb80b06c648013c
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 03:22:36 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,17 @@
 import scraperwiki
 import re
+import urlparse
 from bs4 import BeautifulSoup # BeautifulSoup helps you to find what you want in HTML
+
+def urlEncodeNonAscii(b):
+    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
+
+def iriToUri(iri):
+    parts= urlparse.urlparse(iri)
+    return urlparse.urlunparse(
+        part.encode('idna') if parti==1 else urlEncodeNonAscii(part.encode('utf-8'))
+        for parti, part in enumerate(parts)
+    )
 
 scraperwiki.sqlite.execute("drop table if exists swdata")
 
@@ -13,14 +24,14 @@ bezirke = []
 orte = []
 events = []
 for bezirkarea in soup.find_all("area"): # for each 
-    url = domain + bezirkarea['href']
+    url = iriToUri(domain + bezirkarea['href'])
     title = bezirkarea['title']
     bezirk = title.replace("Bezirk ", "")
     bezirke.append({"name": bezirk, "url": url}) # put the values extracted into a list
     html = scraperwiki.scrape(url) 
     soup = BeautifulSoup(html).find(id="overlay-content")
     for ortli in soup.find_all("li"): 
-        url= domain + ortli.a['href']
+        url=  iriToUri(domain + ortli.a['href'])
         title = ortli.a.contents[0]
         ort = re.sub(' \(.*$', '', title)
         orte.append({"name": ort, "url": url}) # put the values extracted into a list
