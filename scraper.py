bd05c6e29f2be006138e25a572ee049a25a17c53
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 17:38:38 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -103,7 +103,7 @@ for o in orte:
             "besonderheiten": parsefield(ee, "Besonderheiten"),
             "bundesland": parsefield(ee, "Bundesland")
         }
-        print ([obj["id"],obj["bezirk"],obj["ort"],obj["datum"],obj["teilnehmermax"],obj["teilnehmerrelort"]], sep='\t')
+        print (*[obj["id"],obj["bezirk"],obj["ort"],obj["datum"],obj["teilnehmermax"],obj["teilnehmerrelort"]], sep='\t')
         events.append(obj)
         i = i + 1;
     scraperwiki.sqlite.save(unique_keys=["id"], data=events)
