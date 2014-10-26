d9df9adb7c7688f4ca82deed2ecc93caa2c17c03
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 02:31:27 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,8 @@ soup = BeautifulSoup(html) # load the html into beautifulsoup
 bezirke = [] # start with an empty list
 for bezirkarea in soup.find_all("area"): # for each 
     url = bezirkarea['href']
-    name = bezirkarea['title']
-    bezirke.append({"proper": name.replace("Bezirk ", "") , "url": url, "name": name}) # put the values extracted into a list
+    tile = bezirkarea['title']
+    name = title.replace("Bezirk ", "")
+    bezirke.append(name:{"url": url}) # put the values extracted into a list
 
 scraperwiki.sqlite.save(unique_keys=["proper"], data=bezirke)
