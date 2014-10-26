b5d7939c08d05c8808425a83cdb0083bb73bf277
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 02:19:00 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup # BeautifulSoup helps you to find what you want in
 
 url = "http://www.archiv-buergerbewegung.de/index.php/demonstrationen" # loading your url from the csv/database
 html = scraperwiki.scrape(url) # download the html content of the page
-#html = ""
 soup = BeautifulSoup(html) # load the html into beautifulsoup
 
 bezirke = [] # start with an empty list
@@ -12,4 +11,4 @@ for bezirkarea in soup.find_all("area"): # for each
     title = bezirkarea['title']
     bezirke.append({"url": url, "bezirk": title}) # put the values extracted into a list
 
-scraperwiki.sqlite.save(unique_keys=["url"], data=bezirkarea) # save the list of cat photos and captions
+scraperwiki.sqlite.save(unique_keys=["url"], data=bezirkarea)
