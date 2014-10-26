00d34c562b8d81e2554f98e2417cd463583f7238
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2014-10-26 02:10:03 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import scraperwiki
+import re
 from bs4 import BeautifulSoup # BeautifulSoup helps you to find what you want in HTML
 
 scraperwiki.sqlite.execute("drop table if exists swdata")
@@ -7,11 +8,21 @@ url = "http://www.archiv-buergerbewegung.de/index.php/demonstrationen" # loading
 html = scraperwiki.scrape(url) # download the html content of the page
 soup = BeautifulSoup(html) # load the html into beautifulsoup
 
-bezirke = [] # start with an empty list
+bezirke = []
+orte = []
 for bezirkarea in soup.find_all("area"): # for each 
     url = bezirkarea['href']
     title = bezirkarea['title']
     name = title.replace("Bezirk ", "")
-    bezirke.append({"bezirk": name, "url": url}) # put the values extracted into a list
+    bezirke.append({"name": name, "url": url}) # put the values extracted into a list
+    html = scraperwiki.scrape(url) 
+    soup = BeautifulSoup(html)
+    for ortli in soup.find_all("li"): 
+        url = ortli.a['href']
+        title = ortli.a.contents[0]
+        name = re.sub("\(.*$", "", title)
+        count = re.search("\((\d+) Eintrage|Einträge\)$", title)
+        orte.append({"name": name, "url": url, "count": count}) # put the values extracted into a list
 
-scraperwiki.sqlite.save(unique_keys=["bezirk"], data=bezirke)
+    
+scraperwiki.sqlite.save(unique_keys=["name"], data=orte)
